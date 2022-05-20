743052e9eb0e64c761636c8d748e8d1f0233e520
sancho-sumy <59047465+sancho-sumy@users.noreply.github.com>
2022-05-20 18:28:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // todo: add logic
+
+const slides = document.querySelectorAll(".slide");
+
+function activation(event) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = "slide"
+    };
+    event.target.className += " active";
+}
+
+const EventFunction = (res) => {
+    res.onclick = () => activation(event);
+}
+
+slides.forEach(EventFunction);
